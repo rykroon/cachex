9a35d21e7433751e25285eb969a1fe58c9ebe252
Ryan Kroon <rykroon.tech@gmail.com>
2021-03-07 10:30:01 -0500

    added comments

--- cache/connections.py
+++ cache/connections.py
@@ -5,10 +5,16 @@ connection_pool = None
 
 
 def connect(*args, **kwargs):
+    """
+        Create a new Connection Pool
+    """
     global connection_pool
     connection_pool = ConnectionPool(*args, **kwargs)
     return connection_pool
 
 
 def get_client():
+    """
+        Get a connection from the connection pool
+    """
     return Redis(connection_pool=connection_pool)

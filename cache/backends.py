44de7344aa6a1228a156933efb3e459b14d9b2d1
Ryan Kroon <rykroon.tech@gmail.com>
2021-11-29 20:52:35 -0500

    Added RedisBackend

--- cache/backends.py
+++ cache/backends.py
@@ -1,16 +1,19 @@
-
+from cache.sentinels import NoTTL, Missing
 
 
 class Backend:
 
-    def get(self, key, default=None):
+    def __contains__(self, key):
+        raise NotImplementedError
+
+    def get(self, key):
         """
             Returns the value associated with the key.
-            Returns default if the key does not exist.
+            Should return Undefined if the key does not exist
         """
         raise NotImplementedError
 
-    def set(self, key, value, ttl=None):
+    def set(self, key, value, ttl):
         """
             Set the value of `value` to key `key`.
             The key will expire after `ttl` seconds.
@@ -25,6 +28,32 @@ class Backend:
         raise NotImplementedError
 
 
+class AsyncBackend:
+
+    async def get(self, key):
+        raise NotImplementedError
+
+    async def set(self, key, value, ttl):
+        raise NotImplementedError
+
+    async def delete(self, key):
+        raise NotImplementedError
+
+
 class RedisBackend(Backend):
-    ...
+    
+    def __init__(self, client):
+        self.client = client
+
+    def __contains__(self, key):
+        return key in self.client
+
+    def get(self, key):
+        return self.client.get(key)
+
+    def set(self, key, value, ttl):
+        self.client.set(key, value, ex=ttl)
+
+    def delete(self, key):
+        self.client.delete(key)
 

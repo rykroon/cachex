5dc3f97ee4ee24c636f01196ede598ed9e1fd4b7
Ryan Kroon <rykroon.tech@gmail.com>
2021-11-29 20:57:36 -0500

    Added to do.

--- cache/sentinels.py
+++ cache/sentinels.py
@@ -9,3 +9,10 @@ class NotPassed:
 
 class NotFound:
     ...
+
+
+"""
+    To Do:
+        - Create sentinel value NotPassed for differentiating between None and a kwarg that was not passed.
+        - Create sentinel value NotFound for differentiating between None and a key not being found in the cache.
+"""

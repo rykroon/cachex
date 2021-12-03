4b10cffff191513625356311b1e86e7e894fc3ea
Ryan Kroon <rykroon.tech@gmail.com>
2021-12-03 17:59:21 -0500

    Added AsyncCache and AsyncRedisBackend.

--- cache/backends.py
+++ cache/backends.py
@@ -1,4 +1,4 @@
-from cache.sentinels import NoTTL, Missing
+from cache.constants import MissingKey
 
 
 class Backend:
@@ -9,7 +9,7 @@ class Backend:
     def get(self, key):
         """
             Returns the value associated with the key.
-            Should return Undefined if the key does not exist
+            Should return MissingKey if the key does not exist
         """
         raise NotImplementedError
 
@@ -42,14 +42,23 @@ class AsyncBackend:
 
 class RedisBackend(Backend):
     
-    def __init__(self, client):
-        self.client = client
+    def __init__(self, client=None, **kwargs):
+        if client and kwargs:
+            raise ValueError
+
+        if client:
+            self.client = client
+
+        else:
+            import redis
+            self.client = redis.Redis(**kwargs)
 
     def __contains__(self, key):
         return key in self.client
 
     def get(self, key):
-        return self.client.get(key)
+        value = self.client.get(key)
+        return MissingKey if value is None else value
 
     def set(self, key, value, ttl):
         self.client.set(key, value, ex=ttl)
@@ -57,3 +66,26 @@ class RedisBackend(Backend):
     def delete(self, key):
         self.client.delete(key)
 
+
+class AsyncRedisBackend(AsyncBackend):
+
+    def __init__(self, client=None, **kwargs):
+        if client and kwargs:
+            raise ValueError
+
+        if client:
+            self.client = client
+
+        else:
+            import aioredis
+            self.client = aioredis.from_url(**kwargs)
+    
+    async def get(self, key):
+        value = await self.client.get(key)
+        return MissingKey if value is None else value
+
+    async def set(self, key, value, ttl):
+        await self.client.set(key, value, ttl)
+
+    async def delete(self, key):
+        await self.client.delete(key)
--- cache/cache.py
+++ cache/cache.py
@@ -1,34 +1,69 @@
 from cache.serializers import StringSerializer
-from cache.sentinels import Undefined
+from cache.constants import DEFAULT_TTL, NotPassed, MissingKey
 
 
 class Cache:
 
-    def __init__(self, namespace=None, ttl=300,  backend=None, serializer=None):
+    def __init__(self, namespace=None, ttl=DEFAULT_TTL,  backend=None, serializer=None):
         self.namespace = namespace
         self.ttl = ttl
         self._backend = backend
         self._serializer = serializer or StringSerializer()
 
     def __contains__(self, key):
+        key = self._build_key(key)
         return key in self._backend
 
     def _build_key(self, key):
-        return f'{self.namespace}{key}' if self.namespace else key
+        return f'{self.namespace}:{key}' if self.namespace else key
 
     def get(self, key, default=None):
         key = self._build_key(key)
         value = self._backend.get(key)
-        value = self._serializer.loads(value)
-        return value if value is not None else default
+        if value is MissingKey:
+            return default
+        return self._serializer.loads(value)
 
-    def set(self, key, value, ttl=Undefined):
+    def set(self, key, value, ttl=NotPassed):
         key = self._build_key(key)
         value = self._serializer.dumps(value)
-        ttl = self.ttl if ttl is Undefined else ttl
+        ttl = self.ttl if ttl is NotPassed else ttl
         self._backend.set(key, value, ttl)
 
     def delete(self, key):
         key = self._build_key(key)
         self._backend.delete(key)
+
+
+class AsyncCache:
+
+    def __init__(self, namespace=None, ttl=DEFAULT_TTL,  backend=None, serializer=None):
+        self.namespace = namespace
+        self.ttl = ttl
+        self._backend = backend
+        self._serializer = serializer or StringSerializer()
+
+    def __contains__(self, key):
+        key = self._build_key(key)
+        return key in self._backend
+
+    def _build_key(self, key):
+        return f'{self.namespace}:{key}' if self.namespace else key
+
+    async def get(self, key, default=None):
+        key = self._build_key(key)
+        value = await self._backend.get(key)
+        if value is MissingKey:
+            return default
+        return self._serializer.loads(value)
+
+    async def set(self, key, value, ttl=NotPassed):
+        key = self._build_key(key)
+        value = self._serializer.dumps(value)
+        ttl = self.ttl if ttl is NotPassed else ttl
+        await self._backend.set(key, value, ttl)
+
+    async def delete(self, key):
+        key = self._build_key(key)
+        await self._backend.delete(key)
 

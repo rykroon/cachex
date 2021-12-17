a81057f3d5e66b53885bac0437016e99c48f0aef
Ryan Kroon <rykroon.tech@gmail.com>
2021-12-17 08:56:21 -0500

    Added key_builder kwarg and _default_key_builder.

--- cache/cache.py
+++ cache/cache.py
@@ -4,34 +4,36 @@ from cache.constants import DEFAULT_TTL, NotPassed, MissingKey
 
 class Cache:
 
-    def __init__(self, namespace=None, ttl=DEFAULT_TTL,  backend=None, serializer=None):
+    def __init__(self, namespace=None, key_builder=None, ttl=DEFAULT_TTL,  backend=None, serializer=None):
         self.namespace = namespace
+        self.key_builder = key_builder or self._default_key_builder
         self.ttl = ttl
         self._backend = backend
         self._serializer = serializer or StringSerializer()
 
     def __contains__(self, key):
-        key = self._build_key(key)
+        key = self.key_builder(key, self.namespace)
         return key in self._backend
 
-    def _build_key(self, key):
-        return f'{self.namespace}:{key}' if self.namespace else key
+    def _default_key_builder(self, key, namespace=None):
+        ns = namespace if namespace is not None else self.namespace
+        return f'{ns}:{key}' if ns is not None else key
 
     def get(self, key, default=None):
-        key = self._build_key(key)
+        key = self.key_builder(key, self.namespace)
         value = self._backend.get(key)
         if value is MissingKey:
             return default
         return self._serializer.loads(value)
 
     def set(self, key, value, ttl=NotPassed):
-        key = self._build_key(key)
+        key = self.key_builder(key, self.namespace)
         value = self._serializer.dumps(value)
         ttl = self.ttl if ttl is NotPassed else ttl
         self._backend.set(key, value, ttl)
 
     def delete(self, key):
-        key = self._build_key(key)
+        key = self.key_builder(key, self.namespace)
         self._backend.delete(key)
 
 

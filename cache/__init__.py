9b3319b6cb2206893b83b4d37570f65034fd778e
Ryan Kroon <rykroon.tech@gmail.com>
2021-12-24 14:44:00 -0500

    Added serializers and backends to __init__.py

--- cache/__init__.py
+++ cache/__init__.py
@@ -1,1 +1,3 @@
 from cache.cache import Cache, AsyncCache
+from cache.backends import LocalBackend, RedisBackend, AsyncRedisBackend
+from cache.serializers import PassthroughSerializer, StringSerializer, JsonSerializer, PickleSerializer

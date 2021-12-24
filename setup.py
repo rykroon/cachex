edc97b09987095c08e0e9083b8eb1e72fb3dbe10
Ryan Kroon <rykroon.tech@gmail.com>
2021-12-24 14:40:56 -0500

    Use date as version number.

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
+from datetime import date
 from setuptools import setup, find_packages
+
+# python setup.py sdist
+version = date.today().isoformat().replace('-', '.')
 
 setup(
     name='cache',
-    version='1.0.0',
+    version=version,
     author='Ryan Kroon',
     author_email='rykroon.tech@gmail.com',
-    packages=find_packages(),
-    install_requires=['redis', 'aioredis']
+    packages=find_packages()
 )

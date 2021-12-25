256bb42f887a0a6bb8b646984bfc853c3fb3ec06
Ryan Kroon <rykroon.tech@gmail.com>
2021-12-25 09:34:29 -0500

    Added DummyBackend.

---
+++ cache/backends/dummy.py
@@ -0,0 +1,23 @@
+from cache.backends.base import BaseBackend
+from cache.constants import MissingKey
+
+
+class DummyBackend(BaseBackend):
+
+    def get(self, key):
+        return MissingKey
+
+    def set(self, key, value, ttl):
+        pass
+
+    def delete(self, key):
+        return False
+
+    def has_key(self, key):
+        return False
+
+    def get_ttl(self, key):
+        return MissingKey
+
+    def set_ttl(self, key, ttl):
+        return False

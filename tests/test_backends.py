630fbb7eb6462b1fc81b3036832384bd2a89bcc8
Ryan Kroon <rykroon.tech@gmail.com>
2022-01-08 19:27:40 -0500

    Added tests for add() and x_many() methods.

--- tests/test_backends.py
+++ tests/test_backends.py
@@ -75,45 +75,37 @@ class TestDummyBackend(unittest.TestCase):
 
 class AbstractBackendTest:
 
-    def test_get_missingkey(self):
+    def test_get_set(self):
         assert self.backend.get('a') is MissingKey
 
-    def test_get_any(self):
-        self.backend.set('a', 1, None)
-        assert self.backend.get('a') == 1
-
-    def test_set_persist(self):
         self.backend.set('a', 1, None)
         assert self.backend.get('a') == 1
         assert self.backend.get_ttl('a') == None
-        
-    def test_set_expire(self):
-        self.backend.set('a', 1, 20)
-        assert self.backend.get('a') == 1
-        assert self.backend.get_ttl('a') == 20
 
-    def test_delete_false(self):
+        self.backend.set('a', 1, 20)
+        assert self.backend.get_ttl('a') == 20
+        assert self.backend.get('a') == 1
+
+    def test_add(self):
+        assert self.backend.add('a', 1, None) == True
+        assert self.backend.add('a', 1, None) == False
+
+        assert self.backend.add('b', 2, 300) == True
+        assert self.backend.add('b', 2, 300) == False
+
+    def test_delete(self):
         assert self.backend.delete('a') == False
 
-    def test_delete_true(self):
         self.backend.set('a', 1, None)
         assert self.backend.delete('a') == True
         assert self.backend.get('a') is MissingKey
         
-    def test_has_key_false(self):
+    def test_has_key(self):
         assert self.backend.has_key('a') == False
-        self.backend.set('a', 1, 1)
-        time.sleep(1)
-        assert self.backend.has_key('a') == False
-
-    def test_has_key_true(self):
         self.backend.set('a', 1, None)
         assert self.backend.has_key('a') == True
 
-        self.backend.set('a', 1, 1)
-        assert self.backend.has_key('a') == True
-
-    def test_set_ttl_and_get_ttl(self):
+    def test_get_set_ttl(self):
         assert self.backend.get_ttl('a') is MissingKey
 
         self.backend.set('a', 1, None)
@@ -122,6 +114,24 @@ class AbstractBackendTest:
         self.backend.set_ttl('a', 300)
         time.sleep(1)
         assert self.backend.get_ttl('a') == 299
+
+    def test_get_set_many(self):
+        assert self.backend.get_many('a', 'b', 'c') == {}
+
+        self.backend.set_many({'a': 1, 'b': 2, 'c': 3}, None)
+        assert self.backend.get_many('a', 'b', 'c') == {'a': 1, 'b': 2, 'c': 3}
+        for k in ['a', 'b', 'c']:
+            assert self.backend.get_ttl(k) is None
+
+        self.backend.set_many({'a': 1, 'b': 2, 'c': 3}, 300)
+        assert self.backend.get_many('a', 'b', 'c') == {'a': 1, 'b': 2, 'c': 3}
+        for k in ['a', 'b', 'c']:
+            assert self.backend.get_ttl(k) == 300
+
+    def test_delete_many(self):
+        self.backend.set_many({'a': 1, 'b': 2, 'c': 3}, None)
+        self.backend.delete_many('a', 'b', 'c')
+        assert self.backend.get_many('a', 'b', 'c') == {}
 
 
 class TestRedisBackend(unittest.TestCase, AbstractBackendTest):

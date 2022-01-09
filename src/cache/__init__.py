dfeb0fc8beacd02868a2e71a668a88fa3a7ae14c
Ryan Kroon <rykroon.tech@gmail.com>
2022-01-08 22:25:11 -0500

    Added __version__ to __init__.py file.

--- src/cache/__init__.py
+++ src/cache/__init__.py
@@ -1,3 +1,6 @@
 from cache.cache import Cache
 from cache.backends import LocalBackend, RedisBackend
 from cache.serializers import PassthroughSerializer, StringSerializer, JsonSerializer, PickleSerializer
+
+
+__version__ = '0.1.0'

f40caa29b31c79b77991e5742436c5a1e77f0768
Ryan Kroon <rykroon.tech@gmail.com>
2022-01-08 23:11:07 -0500

    Fixed get_many bug with DummyBackend.

--- src/cache/backends/dummy.py
+++ src/cache/backends/dummy.py
@@ -20,7 +20,7 @@ class DummyBackend(BaseBackend):
         return False
 
     def get_many(self, *keys):
-        return []
+        return [MissingKey] * len(keys)
 
     def set_many(self, mapping, ttl):
         pass
